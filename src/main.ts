11f20ef0ba0685efc9bc83d1103f4f93783911a4
Daniel254 <Daniel.laskin@gmail.com>
2022-08-30 09:29:16 +0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    cert: fs.readFileSync('./ssl/certificate.crt'),
+    ca: fs.readFileSync('./ssl/ca_bundle.crt'),
+    key: fs.readFileSync('./ssl/private.key'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
